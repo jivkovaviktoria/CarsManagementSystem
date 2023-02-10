5bf74946dfbab6a83652d7bd421a44e245c0ea89
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-10 14:47:57 +0200

    Extracted logic to services

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import './styles.css';
+import {useEffect, useState} from "react";
+import * as carService from './services/CarService';
 
 import {Header} from "./components/common/Header";
 import {Footer} from "./components/common/Footer";
 import {Search} from "./components/search/Search";
 import {CarsSection} from "./components/carsSection/CarsSection";
-import {useEffect, useState} from "react";
-
-const baseUrl = "https://localhost:7251/Cars";
 
 function App() {
     const [cars, setCars] = useState([]);
 
     useEffect(() => {
-        fetch(`${baseUrl}`)
-            .then(response => response.json())
-            .then(result => {
-                setCars(result);
-            });
+        carService.getAll()
+            .then(cars => setCars(cars));
     }, []);
 
     console.log(cars);
